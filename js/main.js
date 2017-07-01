7378268a594c81f307e584dfc7aca7d35683e8e8
Katherine Gallagher <katherinegallagher@fas.harvard.edu>
2017-07-01 17:15:43 -0400

    contact popup

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// Validating Empty Field
+function check_empty() {
+	if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
+		alert("Please complete all fields.");
+	} else {
+		document.getElementById('form').submit();
+		alert("Thanks for your message!");
+	}
+}
+
+//Function To Display Popup
+function div_show() {
+	document.getElementById('contact-popup').style.display = "block";
+}
+//Function to Hide Popup
+function div_hide(){
+	document.getElementById('contact-popup').style.display = "none";
+}
